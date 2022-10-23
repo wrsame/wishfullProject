2f7af2b3718da9b4c7a14752f4e070f417ca2223
wrsame <jawa0262@stud.kea.dk>
2022-10-23 18:21:47 +0200

    DB is up

---
+++ src/main/java/com/example/wishfullproject/database/script1.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE IF NOT EXISTS wishfulldb;
+
+USE wishfulldb;
+
+CREATE TABLE IF NOT EXISTS users
+(
+    firstName   VARCHAR(50),
+    surName     VARCHAR(50),
+    email       VARCHAR(50),
+    password    VARCHAR(50),
+    birthday    VARCHAR(50),
+    phoneNumber VARCHAR(50),
+    userID          int,
+    PRIMARY KEY (userID)
+
+);
+
+
+INSERT INTO users(firstName, surName, email, password, birthday, phoneNumber, userID)
+VALUES ('Mahi', 'Yoga','Mahiyoga@gmail.com','Mahi123','16/11/2002', 20908070,1000 );
+
+SELECT * FROM users;
+
+
+
---
+++ src/main/java/com/example/wishfullproject/database/wishlistScript.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS `1000` (
+
+    wishID int,
+    wishlistTitle  VARCHAR(100),
+    wishTitle VARCHAR(100),
+    price int,
+    link VARCHAR(500),
+    description  VARCHAR(500)
+
+
+
+
+
+
+);
