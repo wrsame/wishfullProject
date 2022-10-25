31b0158a7df660a209ae2f68c6d9e6836aabf6b4
BasmaJawad <basm0035@stud.kea.dk>
2022-10-25 23:12:52 +0200

    add wishlist pop up almost done

--- src/main/java/com/example/wishfullproject/UserPackage/controller/userController.java
+++ src/main/java/com/example/wishfullproject/UserPackage/controller/userController.java
@@ -1,4 +1,34 @@
 package com.example.wishfullproject.UserPackage.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class userController {
+    @Controller
+    public static class UserController {
+
+        @GetMapping("/login")
+        public String login(){
+            return "Login";
+        }
+
+        @GetMapping("/signup")
+        public String signup(){
+            return "SignUp";
+        }
+
+
+
+        @GetMapping("/wishlists")
+        public String viewWishlists(){
+            return "AllWishlists";
+        }
+
+
+        @GetMapping("/addwishlist")
+        public String addWishList(){
+            return "AddWishlist";
+        }
+
+    }
 }
